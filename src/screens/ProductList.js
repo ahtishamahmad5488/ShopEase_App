b73fb874c7f6fa67705ae5b013d453c7d1a02bd6
Ahtisham Ahmad <ahtisham548849@gmail.com>
2024-08-23 12:35:45 +0500

    Change UI of ProductList Screen

--- src/screens/ProductList.js
+++ src/screens/ProductList.js
@@ -1,4 +1,155 @@
-// ProductList.js
+// // ProductList.js
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   Image,
+//   FlatList,
+//   TouchableOpacity,
+//   ScrollView,
+// } from 'react-native';
+// import React, {useEffect, useState} from 'react';
+// import {useSelector, useDispatch} from 'react-redux';
+// import {
+//   addToCart,
+//   addToFavorite,
+//   fetchProductSpecificCategory,
+// } from '../redux/Action';
+// import Loader from '../components/Loader';
+// import {useNavigation} from '@react-navigation/native';
+
+// const ProductList = ({route}) => {
+//   const {categoryId} = route.params;
+//   const [loading, setLoading] = useState(true);
+//   const navigation = useNavigation();
+//   const dispatch = useDispatch();
+//   const products = useSelector(state => state.specific.specific);
+
+//   const handleAddToCart = item => {
+//     dispatch(addToCart(item));
+//   };
+//   const handleAddToFavorite = item => {
+//     dispatch(addToFavorite(item));
+//   };
+
+//   useEffect(() => {
+//     if (categoryId) {
+//       const fetchData = async () => {
+//         await dispatch(fetchProductSpecificCategory(categoryId)); // Fetch products
+//         setLoading(false); // Stop the loader after fetching data
+//       };
+//       fetchData();
+//     }
+//   }, [categoryId, dispatch]);
+
+//   const renderProduct = ({item}) => (
+//     <View style={{flex: 1, paddingBottom: 20}}>
+//       <TouchableOpacity
+//         style={styles.productContainer}
+//         onPress={() => navigation.navigate('ProductDetails', {product: item})}>
+//         <Image source={{uri: item.image}} style={styles.productImage} />
+//         <Text style={styles.productName}>{item.name}</Text>
+//         <Text style={styles.productBrand}>{item.brand}</Text>
+//         <Text style={styles.productPrice}>Rs {item.price}</Text>
+//       </TouchableOpacity>
+//       <View style={{flexDirection: 'row'}}>
+//         <TouchableOpacity
+//           onPress={() => handleAddToCart(item)}
+//           style={styles.addToCartButton}>
+//           <Text style={styles.addToCartText}>Add To Cart</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity
+//           onPress={() => handleAddToFavorite(item)}
+//           style={styles.favoriteButton}>
+//           <Image
+//             style={styles.favoriteIcon}
+//             source={require('../images/favoriteFilled.png')}
+//           />
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+
+//   return (
+//     <ScrollView>
+//       <View style={{flex: 1, padding: 16}}>
+//         <Text style={styles.title}>Products</Text>
+//         {loading ? (
+//           <Loader /> // Show the loader while data is being fetched
+//         ) : (
+//           <FlatList
+//             nestedScrollEnabled={true}
+//             scrollEnabled={false}
+//             data={products}
+//             keyExtractor={item => item.id}
+//             renderItem={renderProduct}
+//             numColumns={2}
+//             columnWrapperStyle={{justifyContent: 'space-between'}}
+//             showsVerticalScrollIndicator={false}
+//           />
+//         )}
+//       </View>
+//     </ScrollView>
+//   );
+// };
+
+// export default ProductList;
+
+// const styles = StyleSheet.create({
+//   title: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: 'black',
+//     marginBottom: 10,
+//   },
+//   productContainer: {
+//     flex: 1,
+//     alignItems: 'flex-start',
+//   },
+//   productImage: {
+//     width: 150,
+//     height: 150,
+//     borderRadius: 10,
+//     backgroundColor: '#f0f0f0',
+//   },
+//   productName: {
+//     marginTop: 6,
+//     fontSize: 14,
+//     fontWeight: '600',
+//     color: 'black',
+//   },
+//   productPrice: {
+//     marginTop: 3,
+//     fontSize: 14,
+//     color: 'black',
+//   },
+//   productBrand: {
+//     marginTop: 3,
+//     fontWeight: '500',
+//     fontSize: 14,
+//     color: 'black',
+//   },
+//   addToCartButton: {
+//     backgroundColor: '#ff6f6f',
+//     borderRadius: 8,
+//     width: '66%',
+//     height: 30,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     marginTop: 6,
+//   },
+//   addToCartText: {
+//     color: 'white',
+//   },
+//   favoriteButton: {
+//     paddingLeft: 10,
+//   },
+//   favoriteIcon: {
+//     height: 38,
+//     width: 38,
+//   },
+// });
+
 import {
   StyleSheet,
   Text,
@@ -35,15 +186,15 @@ const ProductList = ({route}) => {
   useEffect(() => {
     if (categoryId) {
       const fetchData = async () => {
-        await dispatch(fetchProductSpecificCategory(categoryId)); // Fetch products
-        setLoading(false); // Stop the loader after fetching data
+        await dispatch(fetchProductSpecificCategory(categoryId));
+        setLoading(false);
       };
       fetchData();
     }
   }, [categoryId, dispatch]);
 
   const renderProduct = ({item}) => (
-    <View style={{flex: 1, paddingBottom: 20}}>
+    <View style={styles.productWrapper}>
       <TouchableOpacity
         style={styles.productContainer}
         onPress={() => navigation.navigate('ProductDetails', {product: item})}>
@@ -52,7 +203,7 @@ const ProductList = ({route}) => {
         <Text style={styles.productBrand}>{item.brand}</Text>
         <Text style={styles.productPrice}>Rs {item.price}</Text>
       </TouchableOpacity>
-      <View style={{flexDirection: 'row'}}>
+      <View style={styles.actionButtons}>
         <TouchableOpacity
           onPress={() => handleAddToCart(item)}
           style={styles.addToCartButton}>
@@ -71,11 +222,11 @@ const ProductList = ({route}) => {
   );
 
   return (
-    <ScrollView>
-      <View style={{flex: 1, padding: 16}}>
+    <ScrollView style={styles.container}>
+      <View style={styles.innerContainer}>
         <Text style={styles.title}>Products</Text>
         {loading ? (
-          <Loader /> // Show the loader while data is being fetched
+          <Loader />
         ) : (
           <FlatList
             nestedScrollEnabled={true}
@@ -84,7 +235,7 @@ const ProductList = ({route}) => {
             keyExtractor={item => item.id}
             renderItem={renderProduct}
             numColumns={2}
-            columnWrapperStyle={{justifyContent: 'space-between'}}
+            columnWrapperStyle={styles.columnWrapper}
             showsVerticalScrollIndicator={false}
           />
         )}
@@ -96,56 +247,85 @@ const ProductList = ({route}) => {
 export default ProductList;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  innerContainer: {
+    padding: 16,
+  },
   title: {
-    fontSize: 24,
-    fontWeight: '600',
+    fontSize: 26,
+    fontWeight: '700',
     color: 'black',
+    marginBottom: 20,
+  },
+  productWrapper: {
+    flex: 1,
+    paddingBottom: 20,
     marginBottom: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    margin: 2,
+    elevation: 5,
   },
   productContainer: {
     flex: 1,
-    alignItems: 'flex-start',
+    alignItems: 'center',
   },
   productImage: {
     width: 150,
     height: 150,
     borderRadius: 10,
+    marginBottom: 10,
     backgroundColor: '#f0f0f0',
   },
   productName: {
-    marginTop: 6,
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: '600',
     color: 'black',
-  },
-  productPrice: {
-    marginTop: 3,
-    fontSize: 14,
-    color: 'black',
+    textAlign: 'center',
+    marginBottom: 4,
   },
   productBrand: {
-    marginTop: 3,
-    fontWeight: '500',
     fontSize: 14,
-    color: 'black',
+    color: 'gray',
+    fontWeight: '500',
+    marginBottom: 4,
+  },
+  productPrice: {
+    fontSize: 18,
+    color: '#ff6f6f',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  actionButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   addToCartButton: {
     backgroundColor: '#ff6f6f',
     borderRadius: 8,
-    width: '66%',
-    height: 30,
+    width: '70%',
+    height: 40,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 6,
   },
   addToCartText: {
     color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
   },
   favoriteButton: {
-    paddingLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   favoriteIcon: {
-    height: 38,
-    width: 38,
+    height: 40,
+    width: 40,
+  },
+  columnWrapper: {
+    justifyContent: 'space-between',
   },
 });
